8e07d82642fd2853822425143088313319f02ebb
Sam Lavigne <splavigne@gmail.com>
2024-10-01 15:08:09 -0400

    update p5 example

--- examples/llamafile/p5llama/sketch.js
+++ examples/llamafile/p5llama/sketch.js
@@ -1,32 +1,37 @@
-let startButton;
-let rec = new p5.SpeechRec('en-US', gotSpeech);
-rec.continuous = true
+let startButton; // create an empty variable to hold the start button
+let rec = new p5.SpeechRec('en-US', gotSpeech); // set up speech recognition, in english. when speech is recognized, call the "gotSpeech" function
+rec.continuous = true // set speech rec to be continuous, meaning it will continue listening indefinitely  (doesn't always work)
 
 function setup() {
-  noCanvas();
-  background(255);
-  startButton = createButton("Start")
-  startButton.mouseClicked(start)
+  noCanvas(); // we aren't using canvas in this example
+  startButton = createButton("Start") // create the start button, label it "start"
+  startButton.mouseClicked(start) // call the "start" function when you click on the button
 }
 
 function start() {
+  // simple function that just starts speech recognition
   rec.start();
 }
 
 async function gotSpeech() {
-  let userInput = rec.resultString;
+  // function that gets called when speech is recognized
 
-  createElement("p", userInput)
+  let userInput = rec.resultString; // this is the what the user said
 
-  const URL = "http://localhost:8080/v1/chat/completions";
+  createElement("p", userInput); // create a <p> element and put the users' speech in it
 
+  const URL = "http://localhost:8080/v1/chat/completions"; // url for our llamafile
+
+  // required headers to set on the http request to llamafile
   const headers = {
     "Content-Type": "application/json",
     Authorization: "Bearer no-key",
   };
 
+  // this says make a "POST" request to llamafile
   const method = "POST";
 
+  // the data we are sending to llamafile
   const body = JSON.stringify({
     model: "LLaMA_CPP",
     messages: [
@@ -42,15 +47,17 @@ async function gotSpeech() {
     ],
   });
 
+  // send to llamafile!
   const response = await fetch(URL, {
     method,
     headers,
     body,
   });
 
+  // get the llm results
   const results = await response.json();
-
   const returnedText = results.choices[0].message.content;
 
+  // put the results in a <p> element
   createElement("p", returnedText)
 }
